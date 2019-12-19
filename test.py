8add56fa3d6235ed694ec5f4a94361ddf4dfb45d
Navneet Sharma <navtheunstoppable@gmail.com>
2019-12-18 20:08:02 -0800

    Modularized python script using functions

--- test.py
+++ test.py
@@ -1,19 +1,24 @@
 import keras
 from keras.models import load_model
 
-#load the model
-model = load_model('fashion_mnist_weights.h5')
-# summary of the model
-model.summary()
+def test_model():
+    ''' Tests a model using the test dataset '''
+    #load the model
+    model = load_model('fashion_mnist_weights.h5')
+    # summary of the model
+    model.summary()
 
-#laod the Fashion MNIST data
-fashion_mnist = keras.datasets.fashion_mnist
-(_,_),(test_images,test_labels) = fashion_mnist.load_data()
+    #load the Fashion MNIST data
+    fashion_mnist = keras.datasets.fashion_mnist
+    (_,_),(test_images,test_labels) = fashion_mnist.load_data()
+    # reshape and normalize test data
+    test_images = test_images.reshape((10000,28,28,1))
+    test_images = test_images/255.0
 
-test_images = test_images.reshape((10000,28,28,1))
-test_images = test_images/255.0
+    # evaluate model on the test dataset
+    loss, accuracy = model.evaluate(test_images,test_labels)
 
-# evaluate model on the test dataset
-loss, accuracy = model.evaluate(test_images,test_labels)
+    print("Accuracy of the model on test dataset: ", accuracy)
 
-print("Accuracy of the model on test dataset: ", accuracy)
+if __name__ == "__main__":
+    test_model()
